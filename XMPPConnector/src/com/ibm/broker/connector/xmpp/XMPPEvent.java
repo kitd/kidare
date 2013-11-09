7e3178cacac1a18344c2f9dfe76bd0238f8bfd73
hosie <hosiemail@gmail.com>
2013-11-09 22:21:07 +0000

    Workaround for null pointer exception in Event.buildProperties

--- XMPPConnector/src/com/ibm/broker/connector/xmpp/XMPPEvent.java
+++ XMPPConnector/src/com/ibm/broker/connector/xmpp/XMPPEvent.java
@@ -1,5 +1,7 @@
 package com.ibm.broker.connector.xmpp;
 
+import java.util.Properties;
+
 import org.jivesoftware.smack.packet.Packet;
 
 import com.ibm.broker.connector.ByteArrayInputRecord;
@@ -22,5 +24,24 @@ public class XMPPEvent extends Event {
 					.appendByteArray(msg.toXML().getBytes());
 		return inputRecord;
 	}
+	
+	@Override
+    public Properties buildProperties() throws MbException {
+        
+        //super's version of this method causes a null pointer exception
+		//workaround by returning an empty Properties object.
+		//eventually, we should populate this with some of the extra information about the packet
+		//Arguably, most of the packet [meta]data would go into these properties and we would just grab the body in buildInputRecord
+        Properties leValues = new Properties();
+        return leValues;
+		
+    }
+
+    @Override
+    public void confirm() throws MbException {
+        
+        super.confirm();
+        
+    }
 
 }
